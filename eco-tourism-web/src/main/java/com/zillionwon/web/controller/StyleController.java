d5f84734e0c439fd54dbbd381f45aa0b9113a6cc
Scar <admin@2wxk.com>
2024-08-02 03:29:50 +0800

    旅行风格接口部分代码重构

--- eco-tourism-web/src/main/java/com/zillionwon/web/controller/StyleController.java
+++ eco-tourism-web/src/main/java/com/zillionwon/web/controller/StyleController.java
@@ -2,13 +2,13 @@ package com.zillionwon.web.controller;
 
 import com.zillionwon.common.core.domain.R;
 import com.zillionwon.web.domain.Style;
-import com.zillionwon.web.domain.UserFavorites;
-import com.zillionwon.web.mapper.StyleMapper;
 import com.zillionwon.web.service.*;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  * 旅行风格接口
@@ -16,84 +16,91 @@ import java.util.List;
  * @author black
  */
 
+@Slf4j
 @RestController
 @RequestMapping("/style")
 public class StyleController {
-    private final StyleService StyleService;
+    private final StyleService styleService;
 
     @Autowired
     public StyleController(StyleService styleService) {
-        this.StyleService = styleService;
+        this.styleService = styleService;
     }
 
     /**
-     * 获取对应id的风格
+     * 通过 ID 获取旅行风格
+     * @param styleId 风格ID
+     * @return 旅行风格
      */
-    @GetMapping("/Style")
-    public R<Style> getStyleId(@RequestParam Long styleId) {
-        Style style = StyleService.getStyleById(styleId);
-        if (style != null) {
-            System.out.println(style);
-            return R.ok("搜索成功", style);
-        } else return R.ok("搜索失败");
+    @GetMapping("/findById/{styleId}")
+    public R<Style> findById(@PathVariable Long styleId) {
+        try {
+            return R.ok(Objects.requireNonNull(styleService.getStyleById(styleId)));
+        } catch (NullPointerException e) {
+            log.info("styleId 为 {} 的旅行风格不存在", styleId);
+            return R.fail("未找到该旅行风格");
+        }
     }
 
     /**
-     * 搜索对应风格名称的风格
+     * 通过名称获取旅行风格
+     * @param styleName 旅行风格名称
+     * @return 旅行风格
      */
-    @GetMapping("/StyleSearch")
-    public R<Style> searchStyleName(@RequestParam String styleName) {
-        Style style = StyleService.searchStyleByName(styleName);
-        if (style != null) {
-            System.out.println(style);
-            return R.ok("搜索成功", style);
-        } else return R.ok("搜索失败");
+    @GetMapping("/findByName/{styleName}")
+    public R<Style> findByName(@PathVariable String styleName) {
+        try {
+            return R.ok(Objects.requireNonNull(styleService.searchStyleByName(styleName)));
+        } catch (NullPointerException e) {
+            log.info("styleName 为 {} 的旅行风格不存在", styleName);
+            return R.fail("未找到该旅行风格");
+        }
     }
 
     /**
      * 添加新的风格
      */
-    @PostMapping("/StyleAdd")
-    public R<Style> addStyle(@RequestParam String styleName) {
-        List<Style> styles = StyleService.getAllStyle();
+    @PostMapping
+    public R<Style> addByName(@RequestParam String styleName) {
+        List<Style> styles = styleService.getAllStyle();
         for (Style style : styles) {
             if (style.getStyleName().equals(styleName)) {
-                return R.ok("风格已存在");
+                return R.fail("风格已存在");
             }
         }
-        StyleService.addStyle(styleName);
-        Style newStyle = StyleService.searchStyleByName(styleName);
+        styleService.addStyle(styleName);
+        Style newStyle = styleService.searchStyleByName(styleName);
         return R.ok("添加成功", newStyle);
     }
 
     /**
      * 删除风格
      */
-    @DeleteMapping("/StyleDelete")
-    public R<Style> deleteStyle(@RequestParam String styleName) {
-        List<Style> styles = StyleService.getAllStyle();
+    @DeleteMapping
+    public R<Style> deleteByName(@RequestParam String styleName) {
+        List<Style> styles = styleService.getAllStyle();
         for (Style style : styles) {
             if (style.getStyleName().equals(styleName)) {
                 long styleId = style.getStyleId();
-                StyleService.deleteStyle(styleId);
+                styleService.deleteStyle(styleId);
                 return R.ok("删除成功");
             }
         }
-        return R.ok("风格本身不存在");
+        return R.fail("风格本身不存在");
     }
 
     /**
      * 更改风格名字
      */
-    @PutMapping("/StyleUpdate")
+    @PutMapping
     public R<String> updateStyleName(@RequestParam Long styleId, String styleName) {
-        List<Style> styles = StyleService.getAllStyle();
+        List<Style> styles = styleService.getAllStyle();
         for (Style style : styles) {
             if (style.getStyleId().equals(styleId)) {
-                StyleService.updateStyleName(styleId, styleName);
-                return R.ok("更改成功", styleName);
+                styleService.updateStyleName(styleId, styleName);
+                return R.ok("修改成功", styleName);
             }
         }
-        return R.ok("风格本身不存在");
+        return R.fail("风格本身不存在");
     }
 }
