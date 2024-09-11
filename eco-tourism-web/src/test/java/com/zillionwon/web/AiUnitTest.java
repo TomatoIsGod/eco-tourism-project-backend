34055bdcec3898ac520df9ef814bfa4ae8f86264
Scar <admin@2wxk.com>
2024-09-11 15:27:04 +0800

    完善 AiUnitTest 测试用例

--- eco-tourism-web/src/test/java/com/zillionwon/web/AiUnitTest.java
+++ eco-tourism-web/src/test/java/com/zillionwon/web/AiUnitTest.java
@@ -1,6 +1,7 @@
 package com.zillionwon.web;
 
 import com.zillionwon.common.langchain.factory.AiModelFactory;
+import dev.langchain4j.model.chat.ChatLanguageModel;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,6 +13,7 @@ public class AiUnitTest {
     @DisplayName("AI 配置项测试")
     @Test
     void configTest() {
-        System.out.println(AiModelFactory.getInstance("zhipu"));
+        ChatLanguageModel model = AiModelFactory.getInstance("zhipu");
+        System.out.println(model.generate("你好"));
     }
 }
